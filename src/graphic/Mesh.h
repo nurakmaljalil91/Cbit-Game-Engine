2c1fdcf7045ddff0bbd1ab40fb6066bd25c7e881
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-01-23 01:02:03 +0800

    New renderer

--- src/graphic/Mesh.h
+++ src/graphic/Mesh.h
@@ -4,15 +4,34 @@
 #include <vector>
 #include <string>
 #include "Renderer.h"
+#include "VertexArray.h"
+#include "Texture.h"
+#include <fstream>
+#include <sstream>
+#include "rapidjson/document.h"
+#include "SDL2/SDL_log.h"
+#include "../math/Math.h"
 
 class Mesh
 {
+private:
+    std::vector<Texture *> textures; // textures associated with this mesh
+    VertexArray *vertex_array;       // vertex array associated with this mesh
+    std::string shader_name;         // name of shade specified with this mesh
+    float radius;                    // store object space bounding sphere radius
+    float specular_power;            // specular power of surface
 public:
-    Mesh();
-    ~Mesh();
+    Mesh();  // Constructor
+    ~Mesh(); // Deconstructor
 
-    bool Load(const &std::string &filename, Renderer *renderer); // Load Mesh
-    void Unload();                                               // Unload the mesh
-};
+    bool Load(const std::string &filename, Renderer *renderer); // Load Mesh
+    void Unload();                                              // Unload the mesh
+
+    VertexArray *Get_Vertex_Array() { return vertex_array; }    // Get the vertex array
+    Texture *Get_Texture(size_t index);                         // Get the texture from the specific index
+    const std::string &Get_Shader_Name();                       // Get the shader name
+    float Get_Radius() const { return radius; }                 // Get the radius
+    float Get_Specular_Power() const { return specular_power; } // Get the specular power of the mesh
+};                                                              // class Mesh
 
 #endif // MESH_H
--- src/graphic/Renderer.h
+++ src/graphic/Renderer.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include "SDL2/SDL.h"
 #include "../math/Math.h"
+#include "Shader.h"
 
 struct DirectionlLight
 {
@@ -16,6 +17,32 @@ struct DirectionlLight
 
 class Renderer
 {
+private:
+    std::unordered_map<std::string, Texture *> textures; // map of textures loaded
+    std::unordered_map<std::string, Mesh *> meshes;      // map of meshes loaded
+    // std::vector<Sprite> sprites
+    //std::vector<MeshComponent *> mesh_components;
+    class Game *game;
+    Shader *sprite_shader;        // sprite shader
+    VertexArray *sprite_vertices; // Sprite vertex array
+    Shader *mesh_shader;
+
+    // view / projection for 3D shaders
+    Matrix4 view;
+    Matrix4 projection;
+    float screen_width;
+    float screen_height;
+    // Lighting data
+    Vector3 ambient_light;
+    DirectionlLight direction_light;
+
+    // Window
+    SDL_Window *window;    // window
+    SDL_GLContext context; // OpenGl context
+    bool Load_Shader();
+    void Create_Sprite_Vertices();
+    void Set_Light_Uniforms(Shader *shader);
+
 public:
     Renderer(class Game *_game);
     ~Renderer();
@@ -24,10 +51,24 @@ public:
     void Shutdown();
     void Unload_Data();
 
-
     void Render();
 
-    void Add_Sprite()
-}
+    // FIXME: Add function sprite component
+    void Add_Sprite();
+    void Remove_Sprite();
+
+    // FIXME:: Add function for mesh component
+
+    Texture *Get_Texture(const std::string &filename);
+    Mesh *Get_Mesh(const std::string &filename);
+
+    void Set_View_Matrix(const Matrix4 &_view) { view = _view; }
+    void Set_Ambient_Light(const Vector3 &_ambient) { ambient_light = _ambient; }
+    DirectionlLight &Get_Directional_Light() { return direction_light; }
+
+    float Get_Screen_Width() const { return screen_width; }
+    float Get_Screen_Height() const { return screen_height; }
+
+}; // class Renderer
 
 #endif // RENDERER_H
