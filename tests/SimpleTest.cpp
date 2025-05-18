e4d090a5e6a06355612fa35cfc7934cafa9aacf6
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2025-05-19 00:13:39 +0800

    Add Unit Testing

---
+++ tests/SimpleTest.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file   SimpleTest.cpp
+ * @brief  Simple test case for demonstrating a Google Test framework.
+ * @author Nur Akmal bin Jalil
+ * @date   2025-05-18
+ */
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
